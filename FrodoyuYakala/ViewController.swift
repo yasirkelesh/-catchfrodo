902379a604b1f153589c8d3cd3782b1a39338884
Muhammed yasir Keles <mukeles@k2m11s08.42kocaeli.com.tr>
2022-09-13 19:59:20 +0300

    lkf

--- FrodoyuYakala/ViewController.swift
+++ FrodoyuYakala/ViewController.swift
@@ -26,15 +26,29 @@ class ViewController: UIViewController {
     var timer = Timer()
     var random = Timer()
     
-    var score = 0;
+    var score = 0
     var counter = 0
+    var HighScore = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        counter = 50
+        counter = 10
+        score = 0;
         TimeLabel.text = "Time \(counter)"
         ScoreLabel.text = "Score \(score)"
+
+        
+        let storedHighScore = UserDefaults.standard.object(forKey: "highscore")
+        
+        if storedHighScore == nil{
+            HighScore = 0
+            HighScoreLabel.text = "HighScore : \(self.HighScore)"
+        }
+        if let newScore = storedHighScore as? Int{
+                HighScore = newScore
+                HighScoreLabel.text = "HighScore : \(self.HighScore)"
+        }
         
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunction), userInfo: nil, repeats: true)
         
@@ -49,18 +63,42 @@ class ViewController: UIViewController {
         TimeLabel.text = "Time: \(counter)"
         counter -= 1
         
+        if self.score > self.HighScore{
+            self.HighScore = self.score
+            self.HighScoreLabel.text = "HighScore : \(self.HighScore)"
+            UserDefaults.standard.set(self.HighScore, forKey: "highscore")
+        }
+        
+        
         if counter == 0{
             timer.invalidate()
             TimeLabel.text = "Time's Over"
             let alert = UIAlertController(title: "Game Over!", message: "Game Over !", preferredStyle: UIAlertController.Style.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (UIAlertAction) in
-                        print(" ok button clicked")
-                        self.HighScoreLabel.text = "100"
+            let okButton = UIAlertAction(title: "Exit", style: UIAlertAction.Style.default) { [self] (UIAlertAction) in
+                        print(" Exit button clicked")
+                        
+                        UserDefaults.standard.set(ScoreLabel.text!, forKey: "score")
+
+                        exit(0)
                     }
             
-            let RestartButton = UIAlertAction(title: "Restart", style: UIAlertAction.Style.default){
+            let RestartButton = UIAlertAction(title: "Replay", style: UIAlertAction.Style.default){
                 (UIAlertAction) in
-                self.viewDidLoad()
+                
+                self.counter = 10
+                self.score = 0;
+                self.TimeLabel.text = "Time \(self.counter)"
+                self.ScoreLabel.text = "Score \(self.score)"
+                
+                self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.timerFunction), userInfo: nil, repeats: true)
+                self.random = Timer.scheduledTimer(timeInterval: 0.6, target: self, selector: #selector(self.randomFunction), userInfo: nil, repeats: true)
+    
+                
+                
+                
+                
+                
+                
             }
             
                     alert.addAction(okButton)
@@ -71,148 +109,139 @@ class ViewController: UIViewController {
     @objc func randomFunction(){
         
         let randomInt = Int.random(in: 1..<9)
+        Frodo2.image = UIImage(named: "beyaz")
+        Frodo3.image = UIImage(named: "beyaz")
+        Frodo4.image = UIImage(named: "beyaz")
+        Frodo5.image = UIImage(named: "beyaz")
+        Frodo6.image = UIImage(named: "beyaz")
+        Frodo7.image = UIImage(named: "beyaz")
+        Frodo8.image = UIImage(named: "beyaz")
+        Frodo9.image = UIImage(named: "beyaz")
+        Frodo1.image = UIImage(named: "beyaz")
+        
+        Frodo1.isUserInteractionEnabled = false
+        Frodo2.isUserInteractionEnabled = false
+        Frodo3.isUserInteractionEnabled = false
+        Frodo4.isUserInteractionEnabled = false
+        Frodo5.isUserInteractionEnabled = false
+        Frodo6.isUserInteractionEnabled = false
+        Frodo7.isUserInteractionEnabled = false
+        Frodo8.isUserInteractionEnabled = false
+        Frodo9.isUserInteractionEnabled = false
+        
         if randomInt == 1{
+            
+
             Frodo1.image = UIImage(named: "frodo")
-            Frodo2.image = UIImage(named: "beyaz")
-            Frodo3.image = UIImage(named: "beyaz")
-            Frodo4.image = UIImage(named: "beyaz")
-            Frodo5.image = UIImage(named: "beyaz")
-            Frodo6.image = UIImage(named: "beyaz")
-            Frodo7.image = UIImage(named: "beyaz")
-            Frodo8.image = UIImage(named: "beyaz")
-            Frodo9.image = UIImage(named: "beyaz")
             
             Frodo1.isUserInteractionEnabled = true
+
             let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
             Frodo1.addGestureRecognizer(gestureRecognizer)
+     
+ 
       
         }
         if randomInt == 2{
+            
+
             Frodo2.image = UIImage(named: "frodo")
-            Frodo1.image = UIImage(named: "beyaz")
-            Frodo3.image = UIImage(named: "beyaz")
-            Frodo4.image = UIImage(named: "beyaz")
-            Frodo5.image = UIImage(named: "beyaz")
-            Frodo6.image = UIImage(named: "beyaz")
-            Frodo7.image = UIImage(named: "beyaz")
-            Frodo8.image = UIImage(named: "beyaz")
-            Frodo9.image = UIImage(named: "beyaz")
             
             Frodo2.isUserInteractionEnabled = true
-            let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
-            Frodo2.addGestureRecognizer(gestureRecognizer)
+
+                let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
+                Frodo2.addGestureRecognizer(gestureRecognizer)
+        
  
         }
         if randomInt == 3{
+            
+
             Frodo3.image = UIImage(named: "frodo")
-            Frodo1.image = UIImage(named: "beyaz")
-            Frodo2.image = UIImage(named: "beyaz")
-            Frodo4.image = UIImage(named: "beyaz")
-            Frodo5.image = UIImage(named: "beyaz")
-            Frodo6.image = UIImage(named: "beyaz")
-            Frodo7.image = UIImage(named: "beyaz")
-            Frodo8.image = UIImage(named: "beyaz")
-            Frodo9.image = UIImage(named: "beyaz")
             
             Frodo3.isUserInteractionEnabled = true
-            let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
-            Frodo3.addGestureRecognizer(gestureRecognizer)
+
+                let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
+                Frodo3.addGestureRecognizer(gestureRecognizer)
+        
   
         }
         if randomInt == 4{
+            
+
             Frodo4.image = UIImage(named: "frodo")
-            Frodo1.image = UIImage(named: "beyaz")
-            Frodo3.image = UIImage(named: "beyaz")
-            Frodo2.image = UIImage(named: "beyaz")
-            Frodo5.image = UIImage(named: "beyaz")
-            Frodo6.image = UIImage(named: "beyaz")
-            Frodo7.image = UIImage(named: "beyaz")
-            Frodo8.image = UIImage(named: "beyaz")
-            Frodo9.image = UIImage(named: "beyaz")
             
             Frodo4.isUserInteractionEnabled = true
-            let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
-            Frodo4.addGestureRecognizer(gestureRecognizer)
+      
+                let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
+                Frodo4.addGestureRecognizer(gestureRecognizer)
+  
   
         }
         if randomInt == 5{
+            
+
             Frodo5.image = UIImage(named: "frodo")
-            Frodo1.image = UIImage(named: "beyaz")
-            Frodo3.image = UIImage(named: "beyaz")
-            Frodo4.image = UIImage(named: "beyaz")
-            Frodo2.image = UIImage(named: "beyaz")
-            Frodo6.image = UIImage(named: "beyaz")
-            Frodo7.image = UIImage(named: "beyaz")
-            Frodo8.image = UIImage(named: "beyaz")
-            Frodo9.image = UIImage(named: "beyaz")
             
             Frodo5.isUserInteractionEnabled = true
-            let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
-            Frodo5.addGestureRecognizer(gestureRecognizer)
+    
+                let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
+                Frodo5.addGestureRecognizer(gestureRecognizer)
+       
 
         }
         if randomInt == 6{
+            
+
             Frodo6.image = UIImage(named: "frodo")
-            Frodo1.image = UIImage(named: "beyaz")
-            Frodo3.image = UIImage(named: "beyaz")
-            Frodo4.image = UIImage(named: "beyaz")
-            Frodo2.image = UIImage(named: "beyaz")
-            Frodo5.image = UIImage(named: "beyaz")
-            Frodo7.image = UIImage(named: "beyaz")
-            Frodo8.image = UIImage(named: "beyaz")
-            Frodo9.image = UIImage(named: "beyaz")
             
             Frodo6.isUserInteractionEnabled = true
-            let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
-            Frodo6.addGestureRecognizer(gestureRecognizer)
+ 
+                let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
+                Frodo6.addGestureRecognizer(gestureRecognizer)
+     
      
         }
         if randomInt == 7{
+            
+
             Frodo7.image = UIImage(named: "frodo")
-            Frodo1.image = UIImage(named: "beyaz")
-            Frodo3.image = UIImage(named: "beyaz")
-            Frodo4.image = UIImage(named: "beyaz")
-            Frodo2.image = UIImage(named: "beyaz")
-            Frodo6.image = UIImage(named: "beyaz")
-            Frodo5.image = UIImage(named: "beyaz")
-            Frodo8.image = UIImage(named: "beyaz")
-            Frodo9.image = UIImage(named: "beyaz")
+            
             
             Frodo7.isUserInteractionEnabled = true
             let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
             Frodo7.addGestureRecognizer(gestureRecognizer)
+                
+        
             
         }
         if randomInt == 8{
+            
+
             Frodo8.image = UIImage(named: "frodo")
-            Frodo1.image = UIImage(named: "beyaz")
-            Frodo3.image = UIImage(named: "beyaz")
-            Frodo4.image = UIImage(named: "beyaz")
-            Frodo2.image = UIImage(named: "beyaz")
-            Frodo6.image = UIImage(named: "beyaz")
-            Frodo7.image = UIImage(named: "beyaz")
-            Frodo5.image = UIImage(named: "beyaz")
-            Frodo9.image = UIImage(named: "beyaz")
             
             Frodo8.isUserInteractionEnabled = true
+
             let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
             Frodo8.addGestureRecognizer(gestureRecognizer)
+
+               
+            
             
         }
         if randomInt == 9{
+            
+
             Frodo9.image = UIImage(named: "frodo")
-            Frodo1.image = UIImage(named: "beyaz")
-            Frodo3.image = UIImage(named: "beyaz")
-            Frodo4.image = UIImage(named: "beyaz")
-            Frodo2.image = UIImage(named: "beyaz")
-            Frodo6.image = UIImage(named: "beyaz")
-            Frodo7.image = UIImage(named: "beyaz")
-            Frodo8.image = UIImage(named: "beyaz")
-            Frodo5.image = UIImage(named: "beyaz")
             
             Frodo9.isUserInteractionEnabled = true
+
             let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(scorePic))
             Frodo9.addGestureRecognizer(gestureRecognizer)
+
+      
+                
+           
           
         }
         
